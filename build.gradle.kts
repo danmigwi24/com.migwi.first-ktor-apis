d32310cfa006347a8fe504658e7fa1093c7593ea
Dan <kimani.daniel@eclectics.io>
2022-03-27 17:10:53 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.migwi"
+version = "0.0.1"
+application {
+    mainClass.set("com.migwi.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+
+    // Exposed ORM library
+    implementation("org.jetbrains.exposed:exposed-core:0.37.3")
+    implementation("org.jetbrains.exposed:exposed-dao:0.37.3")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.37.3")
+//
+    implementation("com.zaxxer:HikariCP:5.0.1") // JDBC Connection Pool
+    implementation("org.postgresql:postgresql:42.3.3") // JDBC Connector for PostgreSQL
+//
+    implementation("io.ktor:ktor-auth:1.6.8")
+    implementation("io.ktor:ktor-auth-jwt:1.6.8")
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.migwi.first-ktor-apis"
